85637287141b6fff071b16d5f0b85b046e6b9e9e
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-11-05 11:42:04 +0100

    ADD: pylint rc

--- invoice/models.py
+++ invoice/models.py
@@ -1,7 +1,9 @@
+"""Models for invoice app."""
 from django.db.models import Model, CharField, ForeignKey, CASCADE
 
 
 class Address(Model):
+    """Defines any type of address. For vendors as well as customers."""
     street = CharField(max_length=120)
     number = CharField(max_length=120)
     city = CharField(max_length=120)
@@ -9,6 +11,7 @@ class Address(Model):
 
 
 class Vendor(Model):
+    """Defines profiles for the invoicer."""
     name = CharField(max_length=255)
     company_name = CharField(max_length=255, unique=True)
     address: Address = ForeignKey(Address, on_delete=CASCADE)
