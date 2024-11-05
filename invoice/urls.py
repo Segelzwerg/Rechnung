2b7fd7c14b7e5a7e3cfef4ad631ea461a391e106
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-11-05 11:39:42 +0100

    ADD: vendor urls

--- invoice/urls.py
+++ invoice/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from invoice.views import AddressCreateView, AddressUpdateView, AddressDeleteView
+from invoice.views import AddressCreateView, AddressUpdateView, AddressDeleteView, VendorCreateView, \
+    VendorUpdateView, VendorDeleteView
 
 urlpatterns = [
     # ...
     path("address/add/", AddressCreateView.as_view(), name="address-add"),
     path("address/<int:pk>/", AddressUpdateView.as_view(), name="address-update"),
     path("address/<int:pk>/delete/", AddressDeleteView.as_view(), name="address-delete"),
+    path("vendor/add/", VendorCreateView.as_view(), name="vendor-add"),
+    path("vendor/<int:pk>/", VendorUpdateView.as_view(), name="vendor-update"),
+    path("vendor/<int:pk>/delete/", VendorDeleteView.as_view(), name="vendor-delete"),
 ]
