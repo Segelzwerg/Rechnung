79d88b946d7b530a5c00b3595e25da7ebd28945b
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-11-16 12:10:51 +0100

    ADD: test for creating address

--- invoice/tests.py
+++ invoice/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.urls import reverse
+from hypothesis import given
+from hypothesis.extra.django import TestCase
+from hypothesis.strategies import characters, text
 
-# Create your tests here.
+from invoice.models import Address
+
+
+class AddAddressViewTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('address-add')
+
+    @given(text(), text(), text(), text())
+    def test_add_address(self, street, number, city, country):
+        response = self.client.post(self.url, data={
+            'street': street,
+            'number': number,
+            'city': city,
+            'country': country
+        }, follow=True)
+        address = Address.objects.get(street=street, number=number)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(address)
+        self.assertEqual(address.city, city)
+        self.assertEqual(address.country, country)
