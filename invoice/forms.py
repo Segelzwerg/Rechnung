598e75b725df66d564d968231d94d31404a7ebe7
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-11-26 21:33:02 +0100

    ADD: customer form
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- invoice/forms.py
+++ invoice/forms.py
@@ -1,5 +1,5 @@
 """Forms of the invoice app"""
-from django.forms import ModelForm
+from django.forms import ModelForm, Form, CharField
 
 from invoice.models import InvoiceItem
 
@@ -10,3 +10,17 @@ class InvoiceItemForm(ModelForm):
     class Meta:
         model = InvoiceItem
         fields = '__all__'
+
+
+class CustomerForm(Form):
+    """Form for customer including an address."""
+    first_name = CharField(max_length=120)
+    last_name = CharField(max_length=120)
+    email = CharField(max_length=256)
+    address_line_1 = CharField(max_length=200)
+    address_line_2 = CharField(max_length=200, required=False)
+    address_line_3 = CharField(max_length=200, required=False)
+    address_postcode = CharField(max_length=10)
+    address_city = CharField(max_length=120)
+    address_state = CharField(max_length=200, required=False)
+    address_country = CharField(max_length=120)
