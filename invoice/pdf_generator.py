05951aabb1bff13357693ad14203072bca222a20
iTitus <iTitus@users.noreply.github.com>
2024-11-30 16:50:15 +0100

    refactor pdf rendering a bit to make text look more consistent

--- invoice/pdf_generator.py
+++ invoice/pdf_generator.py
@@ -3,7 +3,8 @@
 import reportlab.lib.pagesizes
 from reportlab.lib import colors
 from reportlab.pdfgen import canvas
-from reportlab.platypus import Table
+from reportlab.platypus import Table, Paragraph
+from schwifty import IBAN, BIC
 
 (A4_WIDTH, A4_HEIGHT) = reportlab.lib.pagesizes.A4
 
@@ -14,41 +15,41 @@ def gen_invoice_pdf(invoice, filename_or_io):
     # pylint: disable=too-many-locals,too-many-arguments,too-many-positional-arguments
 
     pdf_object = canvas.Canvas(filename_or_io)
+    pdf_object.setFontSize(12)
 
-    y_top = A4_HEIGHT - 50
-    y_step = 20
-    y_steps = 0
+    def render_lines(x, y, lines):
+        """Render lines."""
+        text = '<br/>'.join(lines)
+        p = Paragraph(text)
+        _, h = p.wrapOn(pdf_object, A4_WIDTH, A4_HEIGHT)
+        y_end = y - h
+        p.drawOn(pdf_object, x, y_end)
+        return y_end
 
-    def next_y():
-        """Calculate the y position of the next line on pdf canvas."""
-        nonlocal y_top, y_step, y_steps
-
-        y = y_top - y_step * y_steps
-        y_steps += 1
-        return y
-
-    def render_address(x, y, address, prefix_lines=(), suffix_lines=(), line_height=20):
+    def render_address(x, y, address, prefix_lines=(), suffix_lines=()):
         """Render an address."""
-        line_count = 0
-        for line in prefix_lines:
-            if line:
-                pdf_object.drawString(x, y - line_count * line_height, line)
-                line_count += 1
-        for line in [
+        all_lines = [line for line in prefix_lines if line] + [line for line in [
             address.line_1,
             address.line_2,
             address.line_3,
             f"{address.postcode} {address.city}",
             address.country
-        ]:
-            if line:
-                pdf_object.drawString(x, y - line_count * line_height, line)
-                line_count += 1
-        for line in suffix_lines:
-            if line:
-                pdf_object.drawString(x, y - line_count * line_height, line)
-                line_count += 1
+        ] if line] + [line for line in suffix_lines if line]
+        return render_lines(x, y, all_lines)
 
+    def render_lines_left_right(x, y, lines, line_offset=0, line_height=16):
+        """Render two-part lines left- and right-aligned.
+        Expects lines to be a tuple with two elements. The first element will be left-aligned, the second element will be right-aligned."""
+        max_width = (max(map(lambda s: pdf_object.stringWidth(s), [row[0] for row in lines])) +
+                     max(map(lambda s: pdf_object.stringWidth(s), [row[1] for row in lines])))
+        if x < 0:
+            x = -x - max_width
+        for i, (text_l, text_r) in enumerate(lines):
+            line_y = y - (i + line_offset) * line_height
+            pdf_object.drawString(x, line_y, text_l)
+            pdf_object.drawRightString(x + max_width, line_y, text_r)
+
+    y_top = A4_HEIGHT - 50
     x_left = 80
 
     # Vendor address
@@ -59,48 +60,44 @@ def gen_invoice_pdf(invoice, filename_or_io):
     render_address(A4_WIDTH - 200, y_top, invoice.customer.address, prefix_lines=[invoice.customer.full_name])
 
     # Title, number, date
-    y_top = A4_HEIGHT - 200
-    pdf_object.drawString(x_left, next_y(), "Invoice")
-    pdf_object.drawString(x_left, next_y(), f"Number: {invoice.invoice_number}")
-    pdf_object.drawString(x_left, next_y(), f"Date: {invoice.date}")
+    title = Paragraph(f"""
+        <font size="16"><b>Invoice</b></font><br/>
+        <font size="12">
+        Number: {invoice.invoice_number}<br/>
+        Date: {invoice.date}
+        </font>
+""")
+    _, h = title.wrapOn(pdf_object, A4_WIDTH, A4_HEIGHT)
+    y_title_end = A4_HEIGHT - 150 - h
+    title.drawOn(pdf_object, x_left, y_title_end)
 
     # Invoice Items
     data = invoice.table_export
     table = Table(
-        data=data,
+        data=[[Paragraph(f"<b>{col}</b>") if i == 0 else str(col) for col in row] for i, row in enumerate(data)],
         style=[
             ("GRID", (0, 0), (-1, -1), 0.5, colors.grey),
             ("LEADING", (0, 0), (-1, 0), 10),
             ("ALIGNMENT", (2, 1), (6, -1), "RIGHT"),
         ]
     )
-    _, table_height = table.wrapOn(pdf_object, 0, 0)
-    table_y_start = next_y()
+    _, table_height = table.wrapOn(pdf_object, A4_WIDTH - 2 * x_left, A4_HEIGHT)
+    table_y_start = y_title_end - 20
     table_y_end = table_y_start - table_height
-    table.drawOn(pdf_object, x=x_left, y=table_y_end)
+    table.drawOn(pdf_object, x_left, table_y_end)
 
     # Totals
-    x_right = A4_WIDTH - 280
-    x_right_2 = A4_WIDTH - 195
-    y_top = table_y_end
-    y_steps = 1
-
-    y = next_y()
-    pdf_object.drawString(x_right, y, "Net Total:")
-    pdf_object.drawAlignedString(x_right_2, y, f"{invoice.net_total:.2f}")
-
-    y = next_y()
-    pdf_object.drawString(x_right, y, "Total:")
-    pdf_object.drawAlignedString(x_right_2, y, f"{invoice.total:.2f}")
+    render_lines_left_right(-(A4_WIDTH - x_left), table_y_end, [
+        ["Net Total: ", f"{invoice.net_total:.2f}"],
+        ["Total: ", f"{invoice.total:.2f}"]
+    ], line_offset=1)
 
     # Tax ID and bank account info
-    y_top = 100
-    y_steps = 0
-    if invoice.vendor.tax_id:
-        pdf_object.drawString(x_left, next_y(), f"Tax ID: {invoice.vendor.tax_id}")
-    if invoice.vendor.bank_account:
-        pdf_object.drawString(x_left, next_y(), f"IBAN: {invoice.vendor.bank_account.iban}")
-        pdf_object.drawString(x_left, next_y(), f"BIC: {invoice.vendor.bank_account.bic}")
+    render_lines_left_right(x_left, 100, [
+        ["Tax ID: ", f"{invoice.vendor.tax_id}"],
+        ["IBAN: ", f"{IBAN(invoice.vendor.bank_account.iban).formatted}"],
+        ["BIC: ", f"{BIC(invoice.vendor.bank_account.bic)}"]
+    ])
 
     pdf_object.showPage()
     pdf_object.save()
