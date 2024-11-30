c8d377e01bc6c68dac2430ba39c918267f261844
iTitus <iTitus@users.noreply.github.com>
2024-11-30 14:18:50 +0100

    clean up pdf generator method

--- invoice/pdf_generator.py
+++ invoice/pdf_generator.py
@@ -1,3 +1,5 @@
+"""PDF generation utilities."""
+
 import reportlab.lib.pagesizes
 from reportlab.lib import colors
 from reportlab.pdfgen import canvas
@@ -6,67 +8,75 @@ from reportlab.platypus import Table
 (A4_WIDTH, A4_HEIGHT) = reportlab.lib.pagesizes.A4
 
 
-def get_y_position(y_print_start, y_step, y_steps):
-    """Calculate the y position of the next line on pdf canvas."""
-    return y_print_start - y_step * y_steps
-
-
 def gen_invoice_pdf(invoice, filename_or_io):
+    """Generate the invoice pdf document."""
     pdf_object = canvas.Canvas(filename_or_io)
-    y_print_start = 800
+
+    y_top = 800
     y_step = 20
     y_steps = 0
-    pdf_object.drawString(100, get_y_position(y_print_start, y_step, y_steps), invoice.vendor.name)
-    y_steps += 1
-    if invoice.vendor.company_name:
-        pdf_object.drawString(100, get_y_position(y_print_start, y_step, y_steps),
-                              invoice.vendor.company_name)
-        y_steps += 1
-    pdf_object.drawString(100, get_y_position(y_print_start, y_step, y_steps),
-                          invoice.vendor.address.line_1)
-    y_steps += 1
-    if invoice.vendor.address.line_2:
-        pdf_object.drawString(100, get_y_position(y_print_start, y_step, y_steps),
-                              invoice.vendor.address.line_2)
-        y_steps += 1
-    if invoice.vendor.address.line_3:
-        pdf_object.drawString(100, get_y_position(y_print_start, y_step, y_steps),
-                              invoice.vendor.address.line_3)
-        y_steps += 1
-    pdf_object.drawString(100, get_y_position(y_print_start, y_step, y_steps),
-                          f'{invoice.vendor.address.postcode} {invoice.vendor.address.city}')
-    y_steps += 1
-    pdf_object.drawString(100, get_y_position(y_print_start, y_step, y_steps),
-                          invoice.vendor.address.country)
-    y_steps += 1
-    pdf_object.drawString(100, get_y_position(y_print_start, y_step, y_steps), 'Rechnung')
-    y_steps += 1
 
+    def next_y():
+        """Calculate the y position of the next line on pdf canvas."""
+        nonlocal y_steps
+
+        y = y_top - y_step * y_steps
+        y_steps += 1
+        return y
+
+    x_left = 100
+
+    # Vendor address
+    pdf_object.drawString(x_left, next_y(), invoice.vendor.name)
+    if invoice.vendor.company_name:
+        pdf_object.drawString(x_left, next_y(), invoice.vendor.company_name)
+    for line in [invoice.vendor.address.line_1, invoice.vendor.address.line_2, invoice.vendor.address.line_3]:
+        if line:
+            pdf_object.drawString(x_left, next_y(), line)
+    pdf_object.drawString(x_left, next_y(), f'{invoice.vendor.address.postcode} {invoice.vendor.address.city}')
+    pdf_object.drawString(x_left, next_y(), invoice.vendor.address.country)
+
+    # Title
+    next_y()
+    pdf_object.drawString(x_left, next_y(), "Rechnung")
+
+    # Invoice Items
     data = invoice.table_export
     table = Table(
         data=data,
         style=[
-            ('GRID', (0, 0), (-1, -1), 0.5, colors.grey),
-            ('LEADING', (0, 0), (-1, 0), 10),
-            ('ALIGNMENT', (2, 1), (6, -1), "RIGHT"),
+            ("GRID", (0, 0), (-1, -1), 0.5, colors.grey),
+            ("LEADING", (0, 0), (-1, 0), 10),
+            ("ALIGNMENT", (2, 1), (6, -1), "RIGHT"),
         ]
     )
     _, table_height = table.wrapOn(pdf_object, 0, 0)
 
-    table_y_start = get_y_position(y_print_start, y_step, y_steps)
-    table.drawOn(pdf_object, x=100, y=table_y_start - table_height)
+    table_y_start = next_y()
+    table.drawOn(pdf_object, x=x_left, y=table_y_start - table_height)
 
-    pdf_object.drawString(A4_WIDTH - 280, table_y_start - table_height - y_step, 'Net Total:')
-    pdf_object.drawAlignedString(A4_WIDTH - 195, table_y_start - table_height - y_step, f'{invoice.net_total:.2f}')
+    # Totals
+    x_right = A4_WIDTH - 280
+    x_right_2 = A4_WIDTH - 195
+    y_top = table_y_start - table_height
+    y_steps = 1
 
-    pdf_object.drawString(A4_WIDTH - 280, table_y_start - table_height - y_step * 2, 'Total:')
-    pdf_object.drawAlignedString(A4_WIDTH - 195, table_y_start - table_height - y_step * 2, f'{invoice.total:.2f}')
+    y = next_y()
+    pdf_object.drawString(x_right, y, "Net Total:")
+    pdf_object.drawAlignedString(x_right_2, y, f"{invoice.net_total:.2f}")
 
+    y = next_y()
+    pdf_object.drawString(x_right, y, "Total:")
+    pdf_object.drawAlignedString(x_right_2, y, f"{invoice.total:.2f}")
+
+    # Tax ID and bank account info
+    y_top = 100
+    y_steps = 0
     if invoice.vendor.tax_id:
-        pdf_object.drawString(100, 100, f'Tax ID: {invoice.vendor.tax_id}')
+        pdf_object.drawString(x_left, next_y(), f"Tax ID: {invoice.vendor.tax_id}")
     if invoice.vendor.bank_account:
-        pdf_object.drawString(100, 80, f'IBAN: {invoice.vendor.bank_account.iban}')
-        pdf_object.drawString(100, 60, f'BIC: {invoice.vendor.bank_account.bic}')
+        pdf_object.drawString(x_left, next_y(), f"IBAN: {invoice.vendor.bank_account.iban}")
+        pdf_object.drawString(x_left, next_y(), f"BIC: {invoice.vendor.bank_account.bic}")
 
     pdf_object.showPage()
     pdf_object.save()
