7ffebc796ecf3ee16e2fc43f1a938c537512b568
iTitus <iTitus@users.noreply.github.com>
2024-12-01 15:52:53 +0100

    add missing admin views

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,7 +1,7 @@
 """Admin registration for invoice app."""
 from django.contrib import admin
 
-from invoice.models import Vendor, Address
+from invoice.models import Vendor, Address, Customer, BankAccount, Invoice, InvoiceItem
 
 
 @admin.register(Address)
@@ -9,6 +9,26 @@ class AddressAdmin(admin.ModelAdmin):
     """Enable to manage address from admin panel."""
 
 
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    """Enable to manage bank account from admin panel."""
+
+
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
     """Enable to manage vendor from admin panel."""
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    """Enable to manage customer from admin panel."""
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    """Enable to manage invoice from admin panel."""
+
+
+@admin.register(InvoiceItem)
+class InvoiceItemAdmin(admin.ModelAdmin):
+    """Enable to manage invoice item from admin panel."""
