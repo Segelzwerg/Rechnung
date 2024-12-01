f8f0c06f291bacec73ffb5078051d022213f03c1
iTitus <iTitus@users.noreply.github.com>
2024-12-01 13:26:24 +0100

    improve printing of decimal numbers in pdf and html

--- invoice/models.py
+++ invoice/models.py
@@ -177,10 +177,21 @@ class InvoiceItem(Model):
     @property
     def list_export(self):
         """Get the fields as list with formatted fields of totals."""
-        return [self.name, self.description, self.quantity, self.price, self.tax_string,
-                f'{self.net_total:.2f}', f'{self.total:.2f}']
+        return [self.name,
+                self.description,
+                self.quantity_string,
+                f'{self.price:.2f}',
+                self.tax_string,
+                f'{self.net_total:.2f}',
+                f'{self.total:.2f}']
+
+    @property
+    def quantity_string(self) -> str:
+        """Get the quantity string."""
+        return f'{self.quantity:.2f}'.rstrip('0').rstrip('.,')
 
     @property
     def tax_string(self) -> str:
         """Get the tax string."""
-        return f'{self.tax * 100}%'
+        s = f'{self.tax * 100:.2f}'.rstrip('0').rstrip('.,')
+        return f'{s}%'
