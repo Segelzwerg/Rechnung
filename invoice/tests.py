1e5486c87d16cb7cf2c5fcef32923943efbadd53
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-12-06 13:23:51 +0100

    FIX: size of owner
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- invoice/tests.py
+++ invoice/tests.py
@@ -75,7 +75,7 @@ def build_address_fields(draw):
 @composite
 def build_bank_fields(draw):
     country_code = draw(sampled_from(['DE', 'AT', 'CH', 'GB', 'LU', 'NL', 'PL', 'SE', 'LT', 'PL']))
-    owner = draw(text())
+    owner = draw(text(min_size=1))
     iban = schwifty.IBAN.random(country_code=country_code, )
     bic = iban.bic
     assume(bic != '')
