4236e584714c5e889e757b66b5e98e0dee236fa5
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2025-04-05 19:44:15 +0200

    FIX: save check
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- invoice/models.py
+++ invoice/models.py
@@ -188,7 +188,7 @@ class Invoice(Model):
     @property
     def items(self):
         """Get list of invoice items. Returns 0 if the invoice is not saved yet."""
-        if self.pk is not None:
+        if self.pk is None:
             return 0
         return list(self.invoiceitem_set.all())
 
