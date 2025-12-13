9a5ae7c03f7a52bf7cc4049764c07ba02525b01c
iTitus <iTitus@users.noreply.github.com>
2025-12-13 17:46:32 +0100

    use deploy checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-python manage.py check
+python manage.py check --deploy
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py compilemessages
-gunicorn --bind=0.0.0.0:8000 rechnung.wsgi:application
+gunicorn --bind=0.0.0.0:8000 rechnung.wsgi
