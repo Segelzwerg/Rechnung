f0abe7878eae74a874a19cd47eacca94014a718c
iTitus <iTitus@users.noreply.github.com>
2025-12-14 14:40:04 +0100

    Fix wrong redirect after logout

--- rechnung/settings.py
+++ rechnung/settings.py
@@ -88,10 +88,10 @@ DATABASES = {"default": env.dj_db_url("DATABASE_URL", default=f"sqlite:///{BASE_
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+    #{"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    #{"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    #{"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    #{"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 # Internationalization
@@ -113,6 +113,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = "staticfiles"
 
 LOGIN_REDIRECT_URL = "start"
+LOGOUT_REDIRECT_URL = "start"
 
 # Settings for django-bootstrap5
 BOOTSTRAP5 = {
