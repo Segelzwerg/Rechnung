53bbe7d14b67a072fc7f35a2d5ff974ef44c6025
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2025-12-28 23:14:01 +0100

    FIX: redirect users to login if they want to access list views
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- invoice/tests.py
+++ invoice/tests.py
@@ -358,6 +358,9 @@ class CustomerListViewTestCase(TestCase):
         self.assertEqual(customer_list[0], customer)
         self.assertNotIn(second_customer, customer_list)
 
+    def test_unauthorized_user_get(self):
+        response = self.client.get(self.url)
+        self.assertRedirects(response, f"/accounts/login/?next={self.url}")
 
 class CustomerDeleteViewTestCase(TestCase):
     @classmethod
@@ -682,6 +685,9 @@ class VendorListViewTestCase(TestCase):
         self.assertEqual(vendors[0], self.vendor)
         self.assertNotIn(second_vendor, vendors)
 
+    def test_unauthorized_user_get(self):
+        response = self.client.get(self.url)
+        self.assertRedirects(response, f"/accounts/login/?next={self.url}")
 
 class VendorDeleteViewTestCase(TestCase):
     @classmethod
@@ -1760,6 +1766,9 @@ class InvoiceListViewTestCase(TestCase):
         self.assertEqual(invoice_list[0], invoice)
         self.assertNotIn(second_invoice, invoice_list)
 
+    def test_unauthorized_user_get(self):
+        response = self.client.get(self.url)
+        self.assertRedirects(response, f"/accounts/login/?next={self.url}")
 
 class InvoiceUpdateViewTestCase(TestCase):
     @classmethod
